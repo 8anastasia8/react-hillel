c65bf23d9f032bc45342e9dec0257d389254f181
8anastasia8 <anastasiaperfilova@yahoo.com>
2022-02-23 00:06:51 +0200

    create-components

---
+++ my-first-app/src/components/main/Main.js
@@ -0,0 +1,11 @@
+import './Main.css';
+
+function Main() {
+    return (
+        <div className="Main">
+            <button className="Main-btn">Main Button</button>
+        </div>
+    )
+}
+
+export default Main;

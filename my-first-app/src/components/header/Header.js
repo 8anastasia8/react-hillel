abedeb04548f522f0f4fa57ad42af71c90ac6561
8anastasia8 <anastasiaperfilova@yahoo.com>
2022-02-23 16:47:06 +0200

    add react-bootstrap

--- my-first-app/src/components/header/Header.js
+++ my-first-app/src/components/header/Header.js
@@ -1,10 +1,43 @@
 import './Header.css';
+import {Navbar, Container, Nav, Form, FormControl, Button} from "react-bootstrap";
+import logo from './icon.png'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Header() {
     return (
-        <div className="Header">
-            <h1>This is header!</h1>
-        </div>
+    <>
+        <Navbar className="Header-navbar" expand="lg" fixed="top">
+            <Container>
+                <Navbar.Brand href="/">
+                    <img
+                        src={logo}
+                        height="30"
+                        width="30"
+                        className="d-inline-block align-top"
+                        alt="logo" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link className="custom-nav" href="/">Home</Nav.Link>
+                        <Nav.Link className="custom-nav" href="/about">About Us</Nav.Link>
+                        <Nav.Link className="custom-nav" href="/contacts">Contacts</Nav.Link>
+                        <Nav.Link className="custom-nav" href="/blog">Blog</Nav.Link>
+                    </Nav>
+                    <Form className="d-flex" >
+                        <FormControl
+                            type="text"
+                            placeholder="Search"
+                            className="me-2"
+                        />
+                        <Button variant="primary" className="custom-btn">
+                            Search
+                        </Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    </>
     )
 }
 
--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './components/header/Header';
 import Main from "./components/main/Main";
+import Footer from "./components/footer/Footer"
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header/>
+      <Header/>
       <Main/>
+      <Footer/>
   </React.StrictMode>,
   document.getElementById('root')
 );

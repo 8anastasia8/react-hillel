c11f1b9646e6097e928013d1ee77daa7b289cbc9
8anastasia8 <anastasiaperfilova@yahoo.com>
2022-02-24 14:11:20 +0200

    add icons

--- my-first-app/src/components/footer/Footer.js
+++ my-first-app/src/components/footer/Footer.js
@@ -1,12 +1,23 @@
 import './Footer.css'
 import { FaInstagram, FaFacebook, FaGithub } from 'react-icons/fa';
+import {Col, Container, Row} from "react-bootstrap";
 
 function Footer() {
 	return (
 		<div className="Footer">
-			<FaInstagram size='2em' color="#fff"/>
-			<FaFacebook size='2em' color="#fff"/>
-			<FaGithub size='2em' color="#fff"/>
+			<Container className="d-flex justify-content-center">
+				<Row>
+					<Col>
+						<a href="/"><FaInstagram className="Footer-icon" size='2em' color="#fff"/></a>
+					</Col>
+					<Col>
+						<a href="/"><FaFacebook className="Footer-icon" size='2em' color="#fff"/></a>
+					</Col>
+					<Col>
+						<a href="/"><FaGithub className="Footer-icon" size='2em' color="#fff"/></a>
+					</Col>
+				</Row>
+			</Container>
 		</div>
 	)
 }

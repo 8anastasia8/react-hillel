a81d18fb0bcaedd2fac7228dba2687c93b01a5a8
Anastasia <90934621+8anastasia8@users.noreply.github.com>
2022-03-28 21:43:41 +0300

    Add files via upload

---
+++ lesson3/src/components/Error.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Error extends Component {
+
+    render() {
+        return (
+            <div className='errorItem'>
+                <p>Error! Please fill in all the required fields</p>
+            </div>
+        );
+    }
+}
+
+export default Error;
